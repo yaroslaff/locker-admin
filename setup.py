f8c078033e27d11ad149e36b63d6c6456dba1644
Yaroslav <yaroslaff@gmail.com>
2021-04-20 23:55:06 +0700

    initial

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+import okerrupdate
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='locker-admin',
+      version='0.1',
+      description='locker-admin',
+      url='https://github.com/yaroslaff/locker-admin',
+      author='Yaroslav Polyakov',
+      author_email='yaroslaff@gmail.com',
+      license='MIT',
+      # packages=['okerrupdate'],
+      scripts=['bin/locker-admin'],
+      # include_package_data=True,
+
+      long_description = read('README.md'),
+      long_description_content_type='text/markdown',
+
+      install_requires=[],
+      zip_safe=False
+      )
+
